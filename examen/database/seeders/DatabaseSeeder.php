1a448664ffddc47880feda69a2fc6a12d0965ecf
Agoitz2 <aezkerrabe21dw@ikzubirimanteo.com>
2022-11-16 09:21:03 +0100

    2 Seeders

--- examen/database/seeders/DatabaseSeeder.php
+++ examen/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(TablaPilotosSeeder::class);
+        $this->call(TablaEscuderiasSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- examen/database/seeders/TablaEscuderiasSeeder.php
+++ examen/database/seeders/TablaEscuderiasSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class TablaEscuderiasSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class TablaEscuderiasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('Escuderias')->insert([
+            'nombre' => 'Escuderia',
+            'fecha_creacion' => '2022-11-16',
+            'num_pilotos' => 2,
+            'sobre_presupuesto' => 1,
+        ]);
+        DB::table('Escuderias')->insert([
+            'nombre' => 'Escuderia1',
+            'fecha_creacion' => '2022-11-16',
+            'num_pilotos' => 1,
+            'sobre_presupuesto' => 1,
+        ]);
     }
 }
